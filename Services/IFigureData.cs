f922e027f46e033d816e7d8c260c98505a27ddc1
Efrem <efremeinikov90@gmail.com>
2026-01-19 11:50:14 +0300

    Введен интерфейс IFigureData для абстракции данных фигур

---
+++ Services/IFigureData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace VectorEditor.Services
+{
+    /// <summary>
+    /// Интерфейс для данных любой фигуры
+    /// </summary>
+    public interface IFigureData
+    {
+        /// <summary>
+        /// Толщина линии фигуры
+        /// </summary>
+        double Thickness { get; set; }
+
+        /// <summary>
+        /// Цвет фигуры
+        /// </summary>
+        Color StrokeColor { get; set; }
+
+        /// <summary>
+        /// Создает фигуру на основе данных
+        /// </summary>
+        Shapes.Figure CreateFigure();
+    }
+}
